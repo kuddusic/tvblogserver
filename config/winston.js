2739da83351d8add9b5fe3ecb374cd13689ab9f9
Kuddusi <kuddusic@gmail.com>
2019-03-06 11:18:19 +0300

    added -mmp prefix

--- config/winston.js
+++ config/winston.js
@@ -90,7 +90,7 @@ batchloggerTransport.on('rotate', function(oldFilename, newFilename) {
 });
 var sendEventsTransport = new myRotateFile({
   name: 'sendEventsTransport',
-  filename : path.join(`${logpath}`, 'iapmonitorlogs','iap-monitor-ptrknb-' + hostname),    
+  filename : path.join(`${logpath}`, 'iapmonitorlogs','iap-monitor-ptrknb-mmp-' + hostname),    
   datePattern : 'YYYYMMDD.HHmm',
   frequency : "15m",    
   json: false,
