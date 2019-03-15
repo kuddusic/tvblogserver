6b02c4694cabe0a3d0a4706dc1ab776aa3d71d27
Kuddusi Ciftcibasi <kuddusic@gmail.com>
2019-03-15 12:04:47 +0300

    Added function for generating text data instead of json bodyParser

--- routes/ratingRoute.js
+++ routes/ratingRoute.js
@@ -2,6 +2,18 @@ var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/ratingController');
 
+function rawBody(req, res, next) {
+    req.setEncoding('utf8');
+    req.body = '';
+    req.on('data', function(chunk) {
+      req.body += chunk;
+    });
+    req.on('end', function(){
+      next();
+    });
+  };
+
+router.use(rawBody);
 /*
 router.use(function(req,res,next){
     console.log("Batch Logger URL: "+ req.url);    
@@ -12,5 +24,6 @@ router.use(function(req,res,next){
 
 router.post('/sendChannelEvents.ajax', controller.logevent);
 router.post('/sendVodEvents.ajax', controller.logevent);
+router.post('/*', controller.logevent);
 
 module.exports = router; 
